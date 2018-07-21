30815d0c20ad99f74b14c3aaf9b0d96ec1d9d148
Steve <scm191@gmail.com>
2018-07-21 15:42:31 -0700

    Initial commit. Base project structure, textures, and tank model.

---
+++ MetalStrike/Assets/Scripts/GridCell.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridCell : MonoBehaviour
+{
+    private Color startColor;
+    private Renderer RenderComponent;
+
+    public Transform tankPrefab;
+
+    private void Start()
+    {
+        RenderComponent = GetComponent<Renderer>();
+    }
+
+    private void OnMouseEnter()
+    {
+        startColor = RenderComponent.material.color;
+        RenderComponent.material.color = Color.yellow;
+    }
+
+    private void OnMouseExit()
+    {
+        RenderComponent.material.color = startColor;
+    }
+
+    private void OnMouseDown()
+    {
+        Instantiate(tankPrefab, transform.position + new Vector3(0, 0.1f, 0), transform.rotation);
+    }
+}
